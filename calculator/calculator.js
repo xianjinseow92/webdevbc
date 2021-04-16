e6df161baea8056bba102f7dc9782f480dd7246b
Xian Jin <xianjinseow92@gmail.com>
2021-04-16 23:07:13 +0800

    complete calculator backend app

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,28 +1,36 @@
 const express = require('express');
+
 const app = express();
+app.use(express.urlencoded({extended: true})); // when we are trying to parse data coming from a HTML form
 const port = 3000; 
 
-
 // Home Page
-const homeContent = `
-    <h1 style="color: #47525e">
-        Hello, World!
-    </h1>
-`
-const homeDocBody = `
-    <body style="background-color: #ededed"> 
-        ${homeContent}
-    </body>
-`
-const homeDoc = `
-    ${homeDocBody}
-`;
-
-
 app.get("/", (req, res) => {
-    res.send(homeDoc);
+    res.sendFile(__dirname + '/index.html');
 });
 
+app.post("/", (req, res) => {
+
+    console.log(req.body); // only available because of express.urlencoded. Allows us to parse HTTP Requests
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    const result = num1 + num2;
+
+    if (isNaN(result)) {
+        res.send(
+            `<h1>You didn't enter numbers, did you?</h1>
+            <h1><a href="/">Click here to go back</a></h1>`
+        );
+    } else {
+        res.send(`
+            <h1>Thanks for sending that.</h1>
+            <h2>Your computed number is ${result}</h2>
+            <h1><a href="/">Click here to go back</a></h1>
+    `);
+    }
+})
+
+// Spin up server on a port
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${[port]}`);
-})
+});
