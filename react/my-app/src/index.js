28250c7017b364dfaee3c4c8ae679c1f8e35d08d
Xian Jin <xianjinseow92@gmail.com>
2021-05-03 20:05:13 +0800

    updates

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,17 +1,109 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Stylesheets
+import './index.scss';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Square extends React.Component {
+  /**
+   * @param {string} value Square button content
+   * @returns 
+   */
+
+  // reactComponents have 2 kinds of in-built fields
+    // @param props -> retrieves values from parent or whatever value is passed into this component
+    // @param state -> sets the data state of this component
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.value
+    };
+  }
+
+  previousValue = this.props.value;
+
+  render() {
+    return (
+      <button 
+        className="square" 
+        onClick={this.setSquareContentToX}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+
+  setSquareContentToX = () => {
+    this.setState({value: 'X'});
+  }
+
+  setSquareContentToXTest = () => {
+    if (this.state.value === 'X') {
+      this.setState({value: this.previousValue});
+    } else {
+      this.setState({value: 'X'});
+    }
+  }
+}
+  
+class Board extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      squares: Array(9).fill(null), // array that corresponds to 9 squares on the board
+    }
+  }
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]}/>;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
---
+++ react/my-app/test/testCode.js
@@ -0,0 +1,5 @@
+const nullArray = Array(9).fill(null);
+const arrTest = Array(10);
+
+console.log(nullArray);
+console.log(arrTest);
