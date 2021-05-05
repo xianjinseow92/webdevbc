1c791f01f134a9737be927542ef4ea6b61f0b3f8
Xian Jin <xianjinseow92@gmail.com>
2021-05-05 17:16:41 +0800

    implement new meetup form

---
+++ react-course-max/routing-http-request-app-wide-state-management/src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,33 @@
+import Card from "../ui/Card";
+
+import classes from "./NewMeetupForm.module.scss";
+
+function NewMeetupForm() {
+  return (
+    <Card>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" id="title" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Image Url</label>
+          <input type="url" id="image" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" id="address" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea type="text" id="description" required rows="5"></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
--- react-course-max/routing-http-request-app-wide-state-management/src/pages/NewMeetup.js
+++ react-course-max/routing-http-request-app-wide-state-management/src/pages/NewMeetup.js
@@ -1,11 +1,16 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    /**
-     * Page that will be loaded by router.
-     * Router package will determine when this page should be loaded
-     */
-    return (
-        <div>New meetup page</div>
-    )
+  /**
+   * Page that will be loaded by router.
+   * Router package will determine when this page should be loaded
+   */
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
