ff27ee453ded3c3662f3142d97034a803c601f3e
Xian Jin <xianjinseow92@gmail.com>
2021-05-17 14:03:15 +0800

    finish basics of react, components, reusable code, passing data, children composition

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/complete-react-guide/src/components/ExpenseItem.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/complete-react-guide/src/components/ExpenseItem.js
@@ -1,7 +1,22 @@
+import classes from "./ExpenseItem.module.scss";
+import { DateTime } from "luxon";
+
 const ExpenseItem = (props) => {
-    return (
-        <h2>Expense Item!</h2>
-    )
-}
+  const expenseDate = DateTime.now().toLocaleString({year: '2-digit', month: 'long', day: 'numeric'});
+  console.log(expenseDate);
+
+  return (
+    <div className={classes["expense-item"]}>
+      {/* <div>18 May 2021</div> */}
+      <div>{expenseDate}</div>
+      <div className={classes["expense-item__description"]}>
+        {/* <h2>{props.title}</h2> */}
+        <h2>Keyboard Parts</h2>
+        {/* <div>{props.amount}</div> */}
+        <div className={classes["expense-item__price"]}>SGD$300</div>
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseItem;
