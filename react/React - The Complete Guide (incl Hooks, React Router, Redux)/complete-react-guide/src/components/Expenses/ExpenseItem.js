650e9d00bb8b3f69d0b730635f48faf7c46559ed
Xian Jin <xianjinseow92@gmail.com>
2021-05-19 13:32:55 +0800

    add lodash and add button to expenseitem

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/complete-react-guide/src/components/Expenses/ExpenseItem.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/complete-react-guide/src/components/Expenses/ExpenseItem.js
@@ -11,6 +11,7 @@ const ExpenseItem = (props) => {
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+        <button>Change Title</button>
       </div>
     </Card>
   );
