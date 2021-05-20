6f82bb6249b2d16e723b3742928b4e17dad96509
Xian Jin <xianjinseow92@gmail.com>
2021-05-20 15:26:57 +0800

    change ExpenseItem to integrate with ExpenseList in a ul > li manner to make more sense semantically

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/complete-react-guide/src/components/Expenses/ExpenseItem.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/complete-react-guide/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   // changing state will cause THIS component to be re-rendered again (re-rendering meaning this WHOLE FUNCTION IS CALLED AGAIN)
   /**
    * Basically, if you have properties that need to be re-evaluated and then re-updated onto the User Interface, you HAVE to use state.
-    * And ONLY THIS COMPONENT will update
+   * And ONLY THIS COMPONENT will update
    */
-  
+
   /**
    * State, Properties, Variables
    */
@@ -20,28 +20,28 @@ const ExpenseItem = (props) => {
    * Functions pertaining to this component
    */
   const clickHandler = () => {
-    setTitle('Poopy Pants'); // changes title variable and re-evaluates the component ExpenseItem again (runs ExpenseItem function)
+    setTitle("Poopy Pants"); // changes title variable and re-evaluates the component ExpenseItem again (runs ExpenseItem function)
     // Will then also re-evaluate the JSX code again
     // any changes detected will then draw any changes it detects (compared to the last time) onto the screen
-    
-    
+
     console.log(title); // you will notice that this line actually doesn't call the NEW variable, but the previous one.
     // this is because useState doesn't change the variable right away.
     // it SCHEDULES this state update
     // therefore, the value of `title` in `console.log(title)` after `setState()` logs the old value, because the value isn't updated yet.
-  }
-
+  };
 
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+          <button onClick={clickHandler}>Change Title</button>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
