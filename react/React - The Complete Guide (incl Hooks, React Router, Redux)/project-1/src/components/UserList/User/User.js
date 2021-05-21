34f0a9d4632eb626c81530a5ccfd57ee7effdc08
Xian Jin <xianjinseow92@gmail.com>
2021-05-21 15:17:34 +0800

    create reusable layout components

---
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/project-1/src/components/UserList/User/User.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./User.module.scss";
+
+const User = props => {
+    return (
+        <li>
+            
+        </li>
+    )
+}
+
+export default User;
---
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/project-1/src/components/UserList/UserList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./UserList";
+
+const UserList = props => {
+    /**
+     * Component essentially serves to render a list of users.
+        * Serves to pass data into a Dumb Component User
+     */
+
+
+    return (
+        <ul>
+            
+        </ul>
+    )
+}
+
+
+export default UserList;
