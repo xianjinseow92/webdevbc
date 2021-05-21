d597a6f3658b0f5f50830b6b9c158fbecbdc7118
Xian Jin <xianjinseow92@gmail.com>
2021-05-21 18:46:23 +0800

    added portal for modal

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/project-1/src/components/Modal/Modal.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/project-1/src/components/Modal/Modal.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
 
 import styles from "./Modal.module.scss";
 
@@ -7,26 +8,29 @@ const Modal = (props) => {
    * Reusable modal to be rendered on screen.
    */
   // Parent props
-  const { errorMsg, closeModal } = props; 
+  const { errorMsg, closeModal } = props;
 
-  return (
-    <>
-      {
-        <div className={styles.modal}>
-          <div className={styles["modal-content"]}>
-            <div className={styles.title}>Invalid Input</div>
-            <div className={styles.body}>
-              <p>{errorMsg}</p>
-            </div>
-            <div className={styles.footer}>
-              <button type="button" onClick={closeModal}>
-                Okay
-              </button>
-            </div>
+  const ModalContent = (props) => {
+    return (
+      <div className={styles.modal}>
+        <div className={styles["modal-content"]}>
+          <div className={styles.title}>Invalid Input</div>
+          <div className={styles.body}>
+            <p>{errorMsg}</p>
+          </div>
+          <div className={styles.footer}>
+            <button type="button" onClick={closeModal}>
+              Okay
+            </button>
           </div>
         </div>
-      }
-    </>
+      </div>
+    );
+  };
+
+  return ReactDOM.createPortal(
+    <ModalContent {...props} />,
+    document.getElementById("modal-root")
   );
 };
 
