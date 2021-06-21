7e60cd2ff8bda5ac4ae8eb613559c8815f526daa
Xian Jin <xianjinseow92@gmail.com>
2021-06-21 20:16:41 +0800

    Complete hooks section

---
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/use-effect-hook/store/auth-context.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {}, // just for autocompletion ppurposes
+  onLogin: (email, password) => {}
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logoutHandler = () => {
+      localStorage.removeItem("isLoggedIn");
+      setIsLoggedIn(false);
+  }
+
+  const loginHandler = () => {
+      localStorage.setItem("isLoggedIn", "1")
+      setIsLoggedIn(true);
+  }
+
+  return <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn,
+      onLogout: logoutHandler,
+      onLogin: loginHandler
+  }}>{props.children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
