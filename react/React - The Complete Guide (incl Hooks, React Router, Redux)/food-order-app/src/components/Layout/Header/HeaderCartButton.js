ee3ed075334c5eb946727c3e2aff4a6c19009ae7
Xian Jin <xianjinseow92@gmail.com>
2021-07-01 21:34:26 +0800

    add bump effect to cartHeaderButton when item added

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/food-order-app/src/components/Layout/Header/HeaderCartButton.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/food-order-app/src/components/Layout/Header/HeaderCartButton.js
@@ -1,28 +1,56 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "store/cart-context";
 import CartIcon from "components/Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const { onClick } = props;
+
+  const [btnIsHighlighted, setBtnIsHightlighted] = useState(false);
   const cartContext = useContext(CartContext);
-  // const numberOfCartItems = cartContext.items.length; // this would be wrong because this is just the number of objects in the array. 
+  const {items} = cartContext;
+  // const numberOfCartItems = cartContext.items.length; // this would be wrong because this is just the number of objects in the array.
   // however, each object represents a food item category (sushi, let's say)
   // which each have an amount of food ordered (number of sushi ordered
-  
+
   // Therefore, we should use a reduce method to access the value of the items
-  const numberOfCartItems = cartContext.items.reduce((accumulatedValue, cartItem) => {
-    return accumulatedValue + cartItem.amount;
-  }, 0);
+  const numberOfCartItems = items.reduce(
+    (accumulatedValue, cartItem) => {
+      return accumulatedValue + cartItem.amount;
+    },
+    0
+  );
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+  
+  
+  useEffect(() => {
+    if (items.length === 0) return; // prevent execution of code below if there are no items in the array
+    setBtnIsHightlighted(true);
 
-  return <button className={classes.button} onClick={onClick}>
+    const timer = setTimeout(() => {
+      setBtnIsHightlighted(false);
+    }, 300);
+
+    // if you add a return function in useEffect, a cleanup function is run
+    return () => {
+      // function run after all code in useEffect run
+      clearTimeout(timer); // to clear the timer
+    }
+
+  }, [items]);
+
+  
+
+  return (
+    <button className={btnClasses} onClick={onClick}>
       <span className={classes.icon}>
-          <CartIcon/>
+        <CartIcon />
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
-  </button>;
+    </button>
+  );
 };
 
 export default HeaderCartButton;
