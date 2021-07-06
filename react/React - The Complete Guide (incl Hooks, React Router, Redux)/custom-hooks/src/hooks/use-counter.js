9506d968560ff5e735ab9bc1457deda7578b3370
Xian Jin <xianjinseow92@gmail.com>
2021-07-06 19:10:07 +0800

    refactor ForwardCounter component with custom useCounter hook

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/custom-hooks/src/hooks/use-counter.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/custom-hooks/src/hooks/use-counter.js
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 
 const useCounter = () => {
+    /**
+     * Custom hooks have THEIR OWN state. Not a shared state. 
+     * So in this example, while it may seem like the 2 timers share the same counter variable 
+        * Becasuse they both increment at the same time
+    * They DO NOT share the same counter state variable.
+     */
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
@@ -11,6 +17,8 @@ const useCounter = () => {
         // cleanup timer 
         return () => clearInterval(interval);
     }, []);
+
+    return counter;
 }
 
 export default useCounter;
