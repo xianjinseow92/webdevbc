b94cfa7cb782b94aa817b0045fc4b747c2ca582d
Xian Jin <xianjinseow92@gmail.com>
2021-07-06 20:29:09 +0800

    refactor code for greater clarity

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/custom-hooks/custom-hooks-realistic/src/App.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/custom-hooks/custom-hooks-realistic/src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -9,19 +9,20 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const fetchTasks = async (taskText) => {
+    // Loading states before getting data
     setIsLoading(true);
     setError(null);
+
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        "https://custom-hooks-69eac-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
       );
 
       if (!response.ok) {
-        throw new Error('Request failed!');
+        throw new Error("Request failed!");
       }
 
       const data = await response.json();
-
       const loadedTasks = [];
 
       for (const taskKey in data) {
@@ -30,7 +31,7 @@ function App() {
 
       setTasks(loadedTasks);
     } catch (err) {
-      setError(err.message || 'Something went wrong!');
+      setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
   };
