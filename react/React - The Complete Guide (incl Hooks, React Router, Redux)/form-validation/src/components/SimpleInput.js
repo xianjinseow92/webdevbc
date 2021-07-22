283f77f0635abdff7c35fc814dcc96831c340cc8
Xian Jin <xianjinseow92@gmail.com>
2021-07-23 00:19:36 +0800

    Add form validation halfway

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/form-validation/src/components/SimpleInput.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/form-validation/src/components/SimpleInput.js
@@ -1,9 +1,28 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  console.log("Component Rendered");
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value)
+  }
+
+  const formSubmitHandler = event => {
+    event.preventDefault(); // prevent browser from sending HTTP request (which reloads site)
+
+    console.log(enteredName); // submit name
+
+    const enteredValue = nameInputRef.current.value; // using useRef
+    console.log("enteredValue: ", enteredValue);
+  }
+
   return (
-    <form>
+    <form  onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputChangeHandler} ref={nameInputRef}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
