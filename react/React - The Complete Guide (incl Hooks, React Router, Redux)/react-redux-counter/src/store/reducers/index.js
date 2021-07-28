c66bca0daa025e77cf6f2df0b2fab7f66022a4af
Xian Jin <xianjinseow92@gmail.com>
2021-07-28 20:40:35 +0800

    introduce new method of managing state with redux toolkit

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/react-redux-counter/src/store/reducers/index.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/react-redux-counter/src/store/reducers/index.js
@@ -5,6 +5,8 @@ import {
   TOGGLE_COUNTER,
 } from "constants/constants";
 
+import { createSlice } from "@reduxjs.toolkit";
+
 // Initial State(s)
 const initialState = {
   counter: 0,
@@ -12,22 +14,41 @@ const initialState = {
   text: "Halo",
 };
 
-/**
- * File that contains all reducers (functions that mutate store)
- */
-export const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return { ...state, counter: state.counter + 1 }; // increase counter by 1
-    case DECREMENT:
-      return { ...state, counter: state.counter - 1 }; // decrease counter by 1
-    case CUSTOM_INCREASE:
-      return { ...state, counter: state.counter + action.value }; // increase counter by a dynamic value
-    case "text":
-      return { ...state, text: "I have changed" };
-    case TOGGLE_COUNTER:
-      return { ...state, showCounter: !state.showCounter };
-    default:
-      return state;
-  }
-};
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    customIncrease(state, action) {
+      state.counter = state.counter + action.value;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+// /**
+//  * File that contains all reducers (functions that mutate store)
+//  */
+// export const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return { ...state, counter: state.counter + 1 }; // increase counter by 1
+//     case DECREMENT:
+//       return { ...state, counter: state.counter - 1 }; // decrease counter by 1
+//     case CUSTOM_INCREASE:
+//       return { ...state, counter: state.counter + action.value }; // increase counter by a dynamic value
+//     case "text":
+//       return { ...state, text: "I have changed" };
+//     case TOGGLE_COUNTER:
+//       return { ...state, showCounter: !state.showCounter };
+//     default:
+//       return state;
+//   }
+// };
