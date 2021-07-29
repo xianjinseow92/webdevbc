fd64ab23c78a8849c429b0f1977ee39369352a84
Xian Jin <xianjinseow92@gmail.com>
2021-07-29 22:17:42 +0800

    create logging of state for all dispatched actions

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/react-redux-async/src/App.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/react-redux-async/src/App.js
@@ -1,11 +1,29 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+
+import { useSelector } from "react-redux";
+
+import { useEffect } from "react";
+import store from "store/index";
 
 function App() {
+
+  // For logging purposes
+  useEffect(() => {
+    store.subscribe(_ => {
+      console.log(store.getState());
+    })
+    return () => {
+      console.log("CLEANUP");
+    }
+  }, []);
+
+  const showCart = useSelector((state) => state.cart.showCart);
+
   return (
     <Layout>
-      <Cart />
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
