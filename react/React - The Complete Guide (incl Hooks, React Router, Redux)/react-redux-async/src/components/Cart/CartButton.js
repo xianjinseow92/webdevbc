0b03aa42976da0e89b4b7ff137fe40f9249cfcd1
Xian Jin <xianjinseow92@gmail.com>
2021-07-30 15:19:54 +0800

    add support for displaying number of items in cart

--- react/React - The Complete Guide (incl Hooks, React Router, Redux)/react-redux-async/src/components/Cart/CartButton.js
+++ react/React - The Complete Guide (incl Hooks, React Router, Redux)/react-redux-async/src/components/Cart/CartButton.js
@@ -1,11 +1,14 @@
 import classes from './CartButton.module.css';
 
+import { useSelector  } from 'react-redux';
+
 const CartButton = (props) => {
   const { onToggleCart } = props;
+  const numItemsInCart = useSelector(state => state.cart.cartItems.length);
   return (
     <button onClick={onToggleCart} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{numItemsInCart}</span>
     </button>
   );
 };
