d5441d3e594d0630ab856f264984298f8b42c8f9
Xian Jin <xianjinseow92@gmail.com>
2021-08-17 20:30:44 +0800

    fetch users on component mount

--- react/complete-react-developer-2021/monsters-rolodex/src/App.js
+++ react/complete-react-developer-2021/monsters-rolodex/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super(); // to initialize Component's constructor
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount = () => {
+    /**
+     * runs once ONLY. only when component mounts.
+     */
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+    console.log("ComponentDidMount");
+  }
+
+  render() {
+    return (
+      <div className="App">
+      {console.log("component rendered")}
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+        <button onClick={this.initializeMonstersHandler}>
+          Create Monsters
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
